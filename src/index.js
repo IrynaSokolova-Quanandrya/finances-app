7d91a3722c836eede1db9d296babe879d96f646a
Iryna <irasokolova25@ukr.net>
2024-10-04 17:17:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <PersistGate persistor={persistor}>
       <Provider store={store}>
         <RouterProvider router={router}>
-          <BrowserRouter basename="/project-finances">
+          <BrowserRouter basename="/finances-app">
             <App />
           </BrowserRouter>
         </RouterProvider>
